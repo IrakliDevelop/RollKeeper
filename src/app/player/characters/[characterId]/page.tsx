8f4ce1156b83ec74edc07ee3c07c93304fc273b7
Irakli Iremashvili <iraklifordev@gmail.com>
2025-08-25 22:50:45 +0400

    fix jackOfAllTrades issue

--- src/app/player/characters/[characterId]/page.tsx
+++ src/app/player/characters/[characterId]/page.tsx
@@ -993,7 +993,7 @@ export default function CharacterSheet() {
                   <input
                     type="checkbox"
                     id="jackOfAllTrades"
-                    checked={character.jackOfAllTrades}
+                    checked={character.jackOfAllTrades ?? false}
                     onChange={toggleJackOfAllTrades}
                     className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2"
                   />
