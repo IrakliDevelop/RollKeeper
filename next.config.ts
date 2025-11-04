d7177653401edd9c1139e59306ac047efc6256b7
Irakli Iremashvili <iraklifordev@gmail.com>
2025-11-04 13:06:32 +0400

    add s3 bucket hostname to next config for images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'rollkeeper-images.s3.eu-central-1.amazonaws.com',
+        port: '',
+        pathname: '/avatars/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
