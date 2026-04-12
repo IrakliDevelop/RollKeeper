1b229203a76a50119d38d4b5967b6ad9c877b801
Irakli Iremashvili <iraklifordev@gmail.com>
2026-04-12 20:04:54 +0400

    refactor: extract spell form constants to spellConstants.ts

--- src/components/SpellManagement.tsx
+++ src/components/SpellManagement.tsx
@@ -53,95 +53,15 @@ import {
   calculateSpellAttackBonus,
   getSpellcastingAbilityModifier,
 } from '@/utils/calculations';
-
-// Constants for spell schools and common casting times
-const SPELL_SCHOOLS = [
-  'Abjuration',
-  'Conjuration',
-  'Divination',
-  'Enchantment',
-  'Evocation',
-  'Illusion',
-  'Necromancy',
-  'Transmutation',
-];
-
-const CASTING_TIMES = [
-  '1 action',
-  '1 bonus action',
-  '1 reaction',
-  '1 minute',
-  '10 minutes',
-  '1 hour',
-  '8 hours',
-  '24 hours',
-];
-
-const RANGES = [
-  'Self',
-  'Touch',
-  '5 feet',
-  '10 feet',
-  '15 feet',
-  '30 feet',
-  '60 feet',
-  '90 feet',
-  '120 feet',
-  '150 feet',
-  '300 feet',
-  '500 feet',
-  '1 mile',
-  'Sight',
-  'Unlimited',
-];
-
-const DURATIONS = [
-  'Instantaneous',
-  '1 round',
-  '1 minute',
-  '10 minutes',
-  '1 hour',
-  '2 hours',
-  '8 hours',
-  '24 hours',
-  '7 days',
-  '10 days',
-  '30 days',
-  'Until dispelled',
-  'Permanent',
-];
-
-const ACTION_TYPES = [
-  { value: '', label: 'No Action Required' },
-  { value: 'attack', label: 'Spell Attack' },
-  { value: 'save', label: 'Saving Throw' },
-  { value: 'utility', label: 'Utility/Other' },
-];
-
-const SAVING_THROWS = [
-  'Strength',
-  'Dexterity',
-  'Constitution',
-  'Intelligence',
-  'Wisdom',
-  'Charisma',
-];
-
-const DAMAGE_TYPES = [
-  'Acid',
-  'Bludgeoning',
-  'Cold',
-  'Fire',
-  'Force',
-  'Lightning',
-  'Necrotic',
-  'Piercing',
-  'Poison',
-  'Psychic',
-  'Radiant',
-  'Slashing',
-  'Thunder',
-];
+import {
+  SPELL_SCHOOLS,
+  CASTING_TIMES,
+  RANGES,
+  DURATIONS,
+  ACTION_TYPES,
+  SAVING_THROWS,
+  DAMAGE_TYPES,
+} from '@/utils/spellConstants';
 
 interface SpellFilters {
   searchQuery: string;
